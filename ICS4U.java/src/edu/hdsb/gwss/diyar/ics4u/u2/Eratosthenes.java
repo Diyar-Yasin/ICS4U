8aa8a24f51beb25b2fc33dc24be37604ddd341f1
Diyar Yasin <47333278+Diyar-Yasin@users.noreply.github.com>
2020-02-20 13:19:51 -0500

    Began Eratosthenes

---
+++ ICS4U.java/src/edu/hdsb/gwss/diyar/ics4u/u2/Eratosthenes.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package edu.hdsb.gwss.diyar.ics4u.u2;
+
+/**
+ *
+ * @author ww
+ */
+public class Eratosthenes {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
