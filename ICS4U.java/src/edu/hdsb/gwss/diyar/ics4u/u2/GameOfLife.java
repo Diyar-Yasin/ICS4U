a738dba30538073f5b34cd562d6fbd52acf0c500
Diyar Yasin <47333278+Diyar-Yasin@users.noreply.github.com>
2020-02-26 14:43:37 -0500

    Was able to randomize the cellGrid on GameOfLife

--- ICS4U.java/src/edu/hdsb/gwss/diyar/ics4u/u2/GameOfLife.java
+++ ICS4U.java/src/edu/hdsb/gwss/diyar/ics4u/u2/GameOfLife.java
@@ -16,40 +16,81 @@ public class GameOfLife {
     /**
      * @param args the command line arguments
      */
-    public static int CELL_ALIVE = 1;
-    public static int CELL_DEAD = 2;
+    static final int CELL_EMPTY = 0;
+    static final int CELL_ALIVE = 1;
+    static final int CELL_DEAD = 2;
 
     public static void main(String[] args) {
 
         // VARIABLES
         int[][] cellGrid = new int[20][20];
+        int[][] prevGenGrid = new int[20][20];
+        int gridSize = cellGrid[0].length * cellGrid.length;
 
         // INPUT
-        copy(cellGrid);
+        cellGrid = startUp(cellGrid, gridSize);
+        prevGenGrid = copy(cellGrid);
+        displayGrid(prevGenGrid);
+        displayGrid(cellGrid);
     }
 
-    public static int[][] start(int[][] cellGrid) {
+    public static int[][] startUp(int[][] cellGrid, int gridSize) {
+
+        // VARIABLES
+        int cellCount;
+        boolean allEmpty = true;
+
+        // OBJECTS
+        Scanner input = new Scanner(System.in);
+
+        // INPUT
+        System.out.println("How many living cells do you want?");
+        cellCount = input.nextInt();
+
+        if (cellCount < gridSize / 2) { //What if they input over 400 cellCount? Or under 0? I have no solution for this case yet
+            cellGrid = addCells(cellCount, cellGrid, allEmpty, gridSize);
+        } else if (cellCount > gridSize / 2) {
+            for (int r = 0; r < 20; r++) {
+                for (int c = 0; c < 20; c++) {
+                    cellGrid[r][c] = 1;
+                }
+            }
+            allEmpty = false;
+            cellGrid = addCells(cellCount, cellGrid, allEmpty, gridSize);
+
+        }
+        return cellGrid;
+    }
+
+    public static int[][] addCells(int cellCount, int[][] cellGrid, boolean allEmpty, int gridSize) {
 
         // VARIABLES
         int row;
         int col;
-        int cellCount;
 
-        // OBJECTS
-        Scanner input = new Scanner(System.in);
-        
         // INPUT
-        System.out.println("How many living cells do you want?");
-        cellCount = input.nextInt();
-        
-        for (int i = 0; i < cellCount; i++){
-            row = (int) (Math.random() * 20 + 1);
-            col = (int) (Math.random() * 20 + 1);
+        if (allEmpty) {
+            for (int i = 0; i < cellCount;) {
+                row = (int) (Math.random() * 19);
+                col = (int) (Math.random() * 19);
 
-            if (cellGrid[row][col] == 0) {
-                cellGrid[row][col] = CELL_ALIVE;
+                if (cellGrid[row][col] == CELL_EMPTY) {
+                    cellGrid[row][col] = CELL_ALIVE;
+                    i++;
+                }
+
             }
+        } else {
+            for (int i = gridSize - cellCount; i > 0;) {
+                row = (int) (Math.random() * 19);
+                col = (int) (Math.random() * 19);
 
+                if (cellGrid[row][col] == CELL_ALIVE) {
+                    cellGrid[row][col] = CELL_EMPTY;
+                    i--;
+                }
+
+            }
         }
 
         return cellGrid;
@@ -69,6 +110,40 @@ public class GameOfLife {
 
         // OUTPUT
         return copy;
+    }
+
+    public static void displayGrid(int[][] cellGrid) {
+
+        for (int row = 0; row < 20; row++) {
+            for (int col = 0; col < 20; col++) {
+                System.out.print(cellGrid[row][col] + " ");
+            }
+            System.out.println("");
+        }
+        System.out.println("");
+    }
+
+    public static void checkCellState(int[][] cellGrid) {
+
+        // VARIABLES
+        int neighCount;
+        int cellState;
+        
+        // INPUT
+        for (int r = 0; r < 20; r++) {
+            for (int c = 0; c < 20; c++) {
+                cellState = cellGrid[r][c];
+                neighCount = countNeigh(cellState);
+                switch neighCount{
+                    case:
+                }
+            }
+        }
+    }
+    
+    public static int countNeigh(int cellState){
+        
+        return cellState;
     }
 
 }
