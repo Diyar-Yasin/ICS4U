50f2fe64764fd911f33a63b3188e33fd8a337c0c
Diyar Yasin <Diyar@Diyars-MacBook-Air.local>
2020-04-08 22:02:51 -0400

    Continued work on PascalsTriangle

--- ICS4U.java/src/edu/hdsb/gwss/diyar/ics4u/u3/PascalsTriangle.java
+++ ICS4U.java/src/edu/hdsb/gwss/diyar/ics4u/u3/PascalsTriangle.java
@@ -5,6 +5,8 @@
  */
 package edu.hdsb.gwss.diyar.ics4u.u3;
 
+import java.util.ArrayList;
+
 /**
  *
  * @author Diyar
@@ -15,30 +17,34 @@ public class PascalsTriangle {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        int n = 4;
-        
-        row(n);
-    }
-    
-    public static int term(int row, int col){
-        //finds the term at row, col
-        int answer = 0;
-        
-        if (row == col || col == 0){
-            answer = 1;
-        }
-        
-        
+        int n = 10;
 
-        return answer;
-    }
-    
-    public static void row(int row){
-        //prints the triangle 
-        
-        if (row >= 0){
-            System.out.println(term(row, row) + " ");
-            row(row - 1);
+        for (int i = 0, j = 0; n > i; i++, j++) {
+            row(i, j);
+            System.out.println("");
         }
+    }
+
+    public static int term(int row, int col) {
+        
+        int term;
+        
+        if (row == 0 || (col == row - 1 )){
+            term = 1;
+        }
+        else{
+            term = 0;
+        }
+        
+        return term;
+    }
+
+    public static int row(int row, int col) {
+
+        System.out.print(term(row, col) + " ");
+        if (row > 0) {
+            row(row - 1, col);
+        }
+        return 0;
     }
 }
